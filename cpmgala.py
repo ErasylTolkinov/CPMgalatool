4fc5df109ad1bec5ea26e1d9c851df3cc8b64c18
CPMgalatool <galatermuxs@gmail.com>
2024-10-20 06:45:30 +0500

    Add files via upload

--- cpmgala.py
+++ cpmgala.py
@@ -2,10 +2,6 @@ import requests
 # import json
 from time import sleep
 
-# Copyright (C) TG:@CPMgala <tg:@gala_original> - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Written by @gala_original <tg:@gala_original>, 05, May, 2024.
 
 BASE_URL: str = "https://red-viper-815239.hostingersite.com/api"
 
